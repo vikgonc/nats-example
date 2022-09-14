b56ac2e15310a98c39b0158e4a08a0634969c141
vgoncharov <vgoncharov@zuzex.com>
2022-09-14 10:29:38 +0300

    Created project structure

---
+++ nats-reply/src/main/java/com/zuzex/natsreply/NatsReplyApplication.java
@@ -0,0 +1,12 @@
+package com.zuzex.natsreply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatsReplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatsReplyApplication.class, args);
+    }
+}
---
+++ nats-request/src/main/java/com/zuzex/natsrequest/NatsRequestApplication.java
@@ -0,0 +1,12 @@
+package com.zuzex.natsrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatsRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NatsRequestApplication.class, args);
+    }
+}
